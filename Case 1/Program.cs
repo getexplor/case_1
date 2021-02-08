3ffb0f28eced4f8672592074f32f94ca1d272ab5
getexplor <agungzohard14@gmail.com>
2021-02-08 11:50:09 +0700

    commit case 1 soal no 1

---
+++ Case 1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Case_1
+{
+    class Program
+    {
+        static int N = 4;
+        const int MaxLength = 100;
+
+        static void Main(string[] args)
+        {
+
+            string pesan = "maafAkuenggak";
+
+            if (pesan.Length < MaxLength)
+            {
+                pesan_rahasia(pesan);
+            }
+            else
+            {
+                Console.WriteLine("Pesan terlalu panjang");
+            }
+            
+            Console.ReadLine();
+        }
+
+        static void pesan_rahasia(string pesan)
+        {
+            int x = 0;
+            int sisa;
+
+            char[,] indexChar = new char[N, N];
+
+            sisa = indexChar.Length - pesan.Length;
+
+            if (sisa != 0)
+            {
+                for (int i = 0; i < sisa; i++)
+                {
+                    pesan = pesan + '*';
+                }
+            }
+
+            char[] data = pesan.ToCharArray();
+
+            Console.WriteLine("Input");
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    indexChar[i, j] = data[x];
+                    x += 1;
+
+                    Console.Write(indexChar[i, j] + " ");
+                }
+                
+                Console.WriteLine();
+            }
+
+            //rotasi 90 ke searah jarum jam
+            for (int i = 0; i < N / 2; i++)
+            {
+                for (int j = i; j < N - i - 1; j++)
+                {
+                    char temp = indexChar[i, j];
+                    indexChar[i, j] = indexChar[N - 1 - j, i];
+                    indexChar[N - 1 - j, i] = indexChar[N - 1 - i, N - 1 - j];
+                    indexChar[N - 1 - i, N - 1 - j] = indexChar[j, N - 1 - i];
+                    indexChar[j, N - 1 - i] = temp;
+                }
+            }
+
+            //print hasil rotasi
+            Console.WriteLine("output");
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (indexChar[i,j] != '*')
+                    {
+                        Console.Write(indexChar[i, j]);
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ Susunan Angka/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Susunan_Angka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka;
+            angka = 4;
+
+            susunAngka(angka);
+            Console.ReadLine();
+        }
+
+        public static void susunAngka(int angka)
+        {
+            int jumlah, temp;
+            temp = angka;
+            int xangka = angka / 2;
+            jumlah = angka;
+
+
+            while (temp != 0)
+            {
+                Console.Write(temp + ",");
+                if (temp == jumlah)
+                {
+                    temp--;
+                }
+                else
+                {
+                    int current = temp;
+
+                    do
+                    {
+                        if (xangka + current > jumlah)
+                        {
+                            xangka--;
+                        }
+                        else
+                        {
+                            Console.Write(xangka + ",");
+                            current += xangka;
+                        }
+                    } while (current != jumlah);
+
+                    if (xangka == 1)
+                    {
+                        temp--;
+                        xangka = jumlah / 2;
+                    }
+                    else
+                    {
+                        xangka--;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
